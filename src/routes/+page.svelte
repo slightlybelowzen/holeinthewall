bad9188a42ad2659f8b5600e7f0f6e667fa4a2a0
Prakhar Nagpal <prakhar.nagpal03@gmail.com>
2024-12-15 23:46:16 +0000

    initial commit with ui

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+    <title>Hole in the Wall</title>
+</svelte:head>
+
+<main class="flex flex-col items-center h-[90vh] my-10 max-w-2xl mx-auto bg-black-elevated border-black-border rounded-3xl border">
+</main>
---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+
+  theme: {
+    extend: {
+      colors: {
+        black: {
+          bg: '#0A0A0A',
+          elevated: '#181818',
+          border: '#2D2D2D'
+        },
+        white: {
+          primary: '#F3F5F7'
+        }
+      },
+      fontFamily: {
+        sans: ['system-ui', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+
+  plugins: []
+} satisfies Config;
